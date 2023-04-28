7d6455fff85f28b1df28e1806bb2508ed67bb048
EthanAmato <ethannamato@gmail.com>
2023-04-28 14:51:35 -0400

    started filtering for api calls

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,10 +1,25 @@
+import { clerkClient } from "@clerk/nextjs/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const postsRouter = createTRPCRouter({
-    // this next getAll is a public procedure that the client calls
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
-  }),
+  // this next getAll is a public procedure that the client calls
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    // take is equivalent of limit in sql, therefore we only will find 100 posts at a time
+    const posts = await ctx.prisma.post.findMany({ 
+      take: 2,
+    });
+    
+
+    //create a string array containing the author ids for the 100 posts we get using the above method
+    const users = await clerkClient.users.getUserList({
+      userId: posts.map((post) => post.authorId),
+      limit: 100,
+    });
+
+    console.log(users)
+
+    return posts
+  })
 });
